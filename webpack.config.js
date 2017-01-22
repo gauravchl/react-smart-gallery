7091b14d3e87b50bfab0115cce916bacd5823e97
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2017-01-22 17:18:02 +0530

    Add watch option

--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,14 @@ demoConfig = {
       },
     ],
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
 
 }
 
